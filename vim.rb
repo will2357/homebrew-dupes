ffaa5883ff4f2cce5e16ca2ca448214075d6ba48
Will Highducheck <will@adaptly.com>
2012-05-14 22:58:18 -0300

    Update vim.rb

--- vim.rb
+++ vim.rb
@@ -4,23 +4,27 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '1d1cce57421c'
-  version '7.3.502'
+  url 'https://vim.googlecode.com/hg/', :revision => '6c318419e331'
+  version '7.3.515'
 
   head 'https://vim.googlecode.com/hg/'
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
-                          "--enable-gui=no",
-                          "--without-x",
-                          "--disable-nls",
-                          "--enable-multibyte",
-                          "--with-tlib=ncurses",
-                          "--enable-pythoninterp",
-                          "--enable-rubyinterp",
+                          #"--enable-gui=no",
+  		                    #"--with-x",
+			                    "--enable-clipboard=yes",
+			                    "--enable-xterm_clipboard=yes",
+			                    "--enable-xterm_save=yes",
+                          #"--disable-nls",
+                          #"--enable-multibyte",
+                          #"--with-tlib=ncurses",
+			                    #"--enable-perlinterp",
+			                    #"--enable-pythoninterp",
+			                    #"--enable-rubyinterp",
                           "--enable-cscope",
-                          "--with-ruby-command=/usr/bin/ruby",
+			                    #"--with-ruby-command=#{`which ruby`.sub(/\n/,'')}",
                           "--with-features=huge"
     system "make"
     system "make install"
